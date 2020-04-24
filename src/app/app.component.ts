e913c0e3a42353bd7cdb4f38bd2765ee4157de97
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-24 11:57:41 +0530

    Displaying articles in sorted order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,13 @@ export class AppComponent {
     constructor() {
         this.articles = [
             new Article ('Angular2', 'http://angular.io', 3),
-            new Article ('Fullstack', 'http://fullstack.io', 2),
-            new Article ('Angular Homepage', 'http://angular.io', 1)
+            new Article ('Fullstack', 'http://fullstack.io', 1),
+            new Article ('Angular Homepage', 'http://angular.io', 2)
         ];
+    }
+
+    sortedArticles() : Article[] {
+        return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
     }
 
     addArticle(title: HTMLInputElement, link: HTMLInputElement) : boolean {
