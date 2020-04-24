784a6e1f0a4e6c564322cef8f3ccf99a9312f8bd
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-24 16:13:28 +0530

    Add Product Department Component

--- src/app/price-display/price-display.component.ts
+++ src/app/price-display/price-display.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'price-display',
-  templateUrl: `<div class="price-display">\${{price}}</div>`,
+  template: `<div class="price-display">\${{price}}</div>`,
   styleUrls: ['./price-display.component.css']
 })
 export class PriceDisplayComponent implements OnInit {
