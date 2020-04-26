b1062ce771b47f59f6dc5e92138e6d004c46a7aa
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-26 16:03:12 +0530

    Add 3 components for navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
