e265e602de4f5c5da3511af9c0db60b9574e270b
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-26 18:11:27 +0530

    Adding Routing - We need to import it in Module - Define the routes using RouterModule and forRoot() - Need to define <router-outlet> - Also define routerLink - routerLink can be defined as a property if we have to set it to some expression like in case of adding parameters. or it could be defined directly without brackets in case of static paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
-    //AppRoutingModule
+    BrowserModule,
+    //AppRoutingModule,
+    RouterModule.forRoot([
+        { path: '', component: HomeComponent },
+        { path: 'home', component: HomeComponent },
+        { path: 'about', component: AboutComponent },
+        { path: 'contact', component: ContactComponent },
+        { path: '**', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
