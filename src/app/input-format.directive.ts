d6968dcc1f428303cb2bc20f99c42812d1f15d96
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-27 08:39:57 +0530

    Extending Functionality - Here we are receiving an input in the directive to set lower or uppercase based on the value - We achieve this using input property

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,16 +1,20 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
+    @Input('format') format;
 
     constructor(private el: ElementRef) { }
 
     @HostListener('blur') onBlur() {
         let value: string = this.el.nativeElement.value;
 
-        this.el.nativeElement.value = value.toLowerCase();
+        if (this.format == 'uppercase')
+            this.el.nativeElement.value = value.toUpperCase();
+        else
+            this.el.nativeElement.value = value.toLowerCase();
     }
 
 }
