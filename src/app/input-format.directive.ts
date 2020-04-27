5353cc73a287411daa26f16e8a88b4c10251f96e
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-27 08:55:00 +0530

    Binding directly to directive name - Since in this case we only have one Input property, we can directly bind it to the directive by using an alias

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -4,13 +4,13 @@ import { Directive, HostListener, ElementRef, Input } from '@angular/core';
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-    @Input('format') format;
+    @Input('appInputFormat') format;
 
     constructor(private el: ElementRef) { }
 
     @HostListener('blur') onBlur() {
         let value: string = this.el.nativeElement.value;
-
+        
         if (this.format == 'uppercase')
             this.el.nativeElement.value = value.toUpperCase();
         else
