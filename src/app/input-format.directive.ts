1782149dc25de0c0f4cc5b2f1ccfdd058f164344
Anshul Dahiya <anshul.rts@gmail.com>
2020-04-27 08:24:11 +0530

    Add basic functionality - Directive is basically used to modify the DOM behaviour - Here, in the directive definition, we are listening to DOM events focus and blur. And then attaching them to functions..

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
 
-  constructor() { }
+    @HostListener('focus') onFocus() {
+        console.log('on Focus');
+    }
+
+    @HostListener('blur') onBlur() {
+        console.log('on Blur');
+    }
+
+    constructor() { }
 
 }
